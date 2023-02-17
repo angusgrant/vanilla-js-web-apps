56a590f88bfdfbab01657bc234916014b7172271
angusgrant <gus.grant@gmail.com>
2023-02-17 20:14:05 +0000

    Week 2 task 2 Cart Count task

--- components/cart.js
+++ components/cart.js
@@ -1,14 +1,41 @@
+import {store} from 'https://cdn.jsdelivr.net/npm/reefjs@12/dist/reef.es.min.js';
+
+// Hold cart data
+let cart = store(JSON.parse(localStorage.getItem('cart')) || {});
 
 
-function storeCart(id) {
-    sessionStorage.setItem(`cart_${id}`, true);
+/**
+ * Add a photo to the cart
+ * @param {String} id The photo ID
+ */
+function addToCart (id) {
+	cart[id] = true;
+	localStorage.setItem('cart', JSON.stringify(cart));
+   
 }
 
-function retrieveCart(id) {
-    return sessionStorage.getItem(`cart_${id}`);
-    
+/**
+ * Check if an item is in the cart
+ * @param  {String}  id The photo ID
+ * @return {Boolean}    If true, the item is in the cart
+ */
+function inCart (id) {
+	return cart[id];
+}
+
+
+/**
+ * Check the count of items in cart
+ * 
+ * @return {Integer}    
+ */
+function getCartCount() {
+    return  `(${Object.keys(cart).length})`;
 }
 
 
 
-export {storeCart, retrieveCart};
+
+
+
+export {addToCart, inCart, getCartCount};
